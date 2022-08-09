9b6943500c707f50ede3e1c78d770b77802f19f1
Lurkars <git@8lh.de>
2022-08-09 08:29:42 +0200

    fix hash mismatch and PWA caching

--- scripts/post-build.js
+++ scripts/post-build.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
 const package_version = require('../package.json').version;
 
-const index_html_path = './dist/gloomhavensecretary/index.html';
+// restore index.html
+const index_html_path = './src/index.html';
 let index_html = fs.readFileSync(index_html_path, 'utf8');
-index_html = index_html.replace('@APP_VERSION@', package_version);
+index_html = index_html.replace(package_version, '@APP_VERSION@');
 fs.writeFile(index_html_path, index_html, 'utf8', (err) => {
   if (err) {
     console.error(err);
   }
 });
 
-const ngsw_json_path = './dist/gloomhavensecretary/ngsw.json';
+// restore ngsw-config.json
+const ngsw_json_path = './ngsw-config.json';
 let ngsw_json = fs.readFileSync(ngsw_json_path, 'utf8');
-ngsw_json = ngsw_json.replace('@APP_VERSION@', package_version);
+ngsw_json = ngsw_json.replace(package_version, '@APP_VERSION@');
 fs.writeFile(ngsw_json_path, ngsw_json, 'utf8', (err) => {
   if (err) {
     console.error(err);
--- scripts/pre-build.js
+++ scripts/pre-build.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const input_dir = './data'
 const output_dir = './src/assets/data'
+const package_version = require('../package.json').version;
 
 const load_subfolder = function (edition_path, folder, default_value) {
   const dir = path.join(edition_path, folder);
@@ -76,3 +77,23 @@ for (edition_path of edition_dirs) {
     }
   });
 }
+
+// add current version to index.html
+const index_html_path = './src/index.html';
+let index_html = fs.readFileSync(index_html_path, 'utf8');
+index_html = index_html.replace('@APP_VERSION@', package_version);
+fs.writeFile(index_html_path, index_html, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
+
+// add current version to ngsw-config.json
+const ngsw_json_path = './ngsw-config.json';
+let ngsw_json = fs.readFileSync(ngsw_json_path, 'utf8');
+ngsw_json = ngsw_json.replace('@APP_VERSION@', package_version);
+fs.writeFile(ngsw_json_path, ngsw_json, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
