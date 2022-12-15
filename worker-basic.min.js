2279435a372a884d339766669377527c0727b391
Lurkars <Lurkars@users.noreply.github.com>
2022-12-15 14:40:11 +0000

    deploy: 7c33e7c7650f410403ce391e7cab87122e03b690

--- worker-basic.min.js
+++ worker-basic.min.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.0.3";
+  var SW_VERSION = "15.0.4";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
