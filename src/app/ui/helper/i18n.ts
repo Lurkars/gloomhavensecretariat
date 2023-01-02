b4ab6368850dd6ff211ac13c047aeaf86d90f798
Lurkars <git@8lh.de>
2023-01-02 14:38:31 +0100

    fix/improve label

--- src/app/ui/helper/i18n.ts
+++ src/app/ui/helper/i18n.ts
@@ -7,16 +7,11 @@ import { EntityValueFunction, EntityValueRegex } from "src/app/game/model/Entity
 export const ghsLabelRegex = /\%((\w+|\.|\-|\:|\%|\+|\|)+)\%/;
 
 export const applyPlaceholder = function (value: string, placeholder: string[] = [], relative: boolean = false, forceFh: boolean = false): string {
-
-  if (settingsManager.settings.fhStyle || forceFh) {
-    return applyFhPlaceholder(value, placeholder, relative);
-  }
-
+  const fh = settingsManager.settings.fhStyle || forceFh;
   while (value.match(ghsLabelRegex)) {
     value = value.replace(ghsLabelRegex, (match, ...args) => {
       let label: string = args[0];
-      const split: string[] = label.split('.');
-      const type = split[1];
+      let split: string[] = label.split('.');
       let value = split[split.length - 1];
 
       if (value.indexOf(':') != 0) {
@@ -31,6 +26,7 @@ export const applyPlaceholder = function (value: string, placeholder: string[] =
         value = "";
       }
 
+      const type = split[1];
       let quotes: boolean = false;
 
       if (match.startsWith("\"") && match.endsWith("\"")) {
@@ -41,12 +37,12 @@ export const applyPlaceholder = function (value: string, placeholder: string[] =
       let image: string = '';
       if (type == "condition") {
         split.splice(0, 1);
-        image = '<img  src="./assets/images/' + split.join('/') + '.svg" class="icon">';
-        replace = '<span class="placeholder-condition">' + settingsManager.getLabel(label) + image + '</span>';
+        image = '<img  src="./assets/images/' + (fh ? 'fh/' : '') + split.join('/') + '.svg" class="icon">';
+        replace = '<span class="placeholder-condition">' + (fh ? '' : settingsManager.getLabel(label)) + image + '</span>';
       } else if (type == "action" && split.length == 3 && !split[2].startsWith('specialTarget') && !split[2].startsWith('summon')) {
         split.splice(0, 1);
-        image = '<img  src="./assets/images/' + split.join('/') + '.svg" class="icon ghs-svg">';
-        replace = '<span class="placeholder-action">' + settingsManager.getLabel(label) + image + value + '</span>';
+        image = '<img  src="./assets/images/' + (fh ? 'fh/' : '') + split.join('/') + '.svg" class="icon ghs-svg">';
+        replace = '<span class="placeholder-action">' + (fh ? '' : settingsManager.getLabel(label)) + image + value + '</span>';
       } else if (type == "element") {
         let element = split[2];
         if (element == "consume") {
@@ -55,11 +51,11 @@ export const applyPlaceholder = function (value: string, placeholder: string[] =
         } else {
           image = '<span class="element inline">';
         }
-        image += '<img src="./assets/images/element/' + element + '.svg"></span>';
+        image += '<img src="./assets/images/' + (fh ? 'fh/' : '') + 'element/' + element + '.svg"></span>';
         replace = image;
       } else if (type == "elementHalf") {
         const elements = value.split('|');
-        replace = '<span class="attack-modifier-effect element-half-placeholder"><span class="element-half-container"><span class="element-half"><img src="./assets/images/element/' + elements[0] + '.svg"></span><span class="element-half"><img src="./assets/images/element/' + elements[1] + '.svg"></span></span></span>';
+        replace = '<span class="attack-modifier-effect element-half-placeholder ' + (fh ? 'fh' : '') + '"><span class="element-half-container"><span class="element-half"><img src="./assets/images/' + (fh ? 'fh/' : '') + 'element/' + elements[0] + '.svg"></span><span class="element-half"><img src="./assets/images/' + (fh ? 'fh/' : '') + 'element/' + elements[1] + '.svg"></span></span></span>';
       } else if (type == "initiative" && split.length == 3) {
         image = '<img class="ghs-svg" src="./assets/images/initiative.svg"></span>'
         replace = '<span class="placeholder-initiative">' + split[2] + image + '</span>';
@@ -94,8 +90,14 @@ export const applyPlaceholder = function (value: string, placeholder: string[] =
         replace = '<span class="map-marker">' + split[2] + '</span>';
       } else if (type == "objectiveMarker" && split.length == 3) {
         replace = '<span class="objective-marker">' + split[2] + '</span>';
-      } else if (type == "damage") {
+      } else if (fh && type == "target" && split.length == 2) {
+        image = '<img  src="./assets/images/fh/action/target.svg" class="icon ghs-svg">';
+        replace = '<span class="placeholder-action">' + image + '</span>';
+      } else if (!fh && type == "damage") {
         replace = '<span class="damage">' + settingsManager.getLabel('game.damage', [value]) + '</span>';
+      } else if (fh && type == "damage") {
+        image = '<img  src="./assets/images/fh/action/damage.svg" class="icon ghs-svg">';
+        replace = '<span class="damage">' + image + value + '</span>';
       } else if (type == "loot" && split.length == 4) {
         image = '<img  src="./assets/images/' + split[3] + '-player.svg" class="icon">';
         replace = '<span class="placeholder-player">' + image + '</span>';
@@ -104,113 +106,6 @@ export const applyPlaceholder = function (value: string, placeholder: string[] =
           applyPlaceholder(settingsManager.getLabel(arg), placeholder, relative));
         labelArgs.push(...placeholder);
         replace = settingsManager.getLabel(label.split(':')[0], labelArgs) + image;
-      }
-
-      return replace;
-    });
-  }
-
-  value = applyValueCalc(value, relative);
-
-  return value;
-}
-
-export const applyFhPlaceholder = function (value: string, placeholder: string[] = [], relative: boolean = false): string {
-  while (value.match(ghsLabelRegex)) {
-    value = value.replace(ghsLabelRegex, (match, ...args) => {
-      let label: string = args[0];
-      let split: string[] = label.split('.');
-      let value = split[split.length - 1];
-
-      if (value.indexOf(':') != 0) {
-        split[split.length - 1] = value.split(':')[0];
-        value = value.split(':')[1];
-        label = split.join('.');
-      } else {
-        value = "";
-      }
-
-      if (!value) {
-        value = "";
-      }
-
-      let type = split[1];
-      let quotes: boolean = false;
-
-      if (match.startsWith("\"") && match.endsWith("\"")) {
-        quotes = true;
-      }
-
-      let replace: string = match;
-      let image: string = '';
-      if (type == "condition") {
-        split.splice(0, 1);
-        image = '<img  src="./assets/images/fh/' + split.join('/') + '.svg" class="icon">';
-        replace = '<span class="placeholder-condition">' + image + '</span>';
-      } else if (type == "action" && split.length == 3 && !split[2].startsWith('specialTarget') && !split[2].startsWith('summon')) {
-        split.splice(0, 1);
-        image = '<img  src="./assets/images/fh/' + split.join('/') + '.svg" class="icon ghs-svg">';
-        replace = '<span class="placeholder-action">' + image + value + '</span>';
-      } else if (type == "element") {
-        let element = split[2];
-        if (element == "consume") {
-          image = '<span class="element inline consume">';
-          element = split[3];
-        } else {
-          image = '<span class="element inline">';
-        }
-        image += '<img src="./assets/images/fh/element/' + element + '.svg"></span>';
-        replace = image;
-      } else if (type == "elementHalf") {
-        const elements = value.split('|');
-        replace = '<span class="attack-modifier-effect element-half-placeholder fh"><span class="element-half-container"><span class="element-half"><img src="./assets/images/fh/element/' + elements[0] + '.svg"></span><span class="element-half"><img src="./assets/images/fh/element/' + elements[1] + '.svg"></span></span></span>';
-      } else if (type == "initiative" && split.length == 3) {
-        image = '<img class="ghs-svg" src="./assets/images/initiative.svg"></span>'
-        replace = '<span class="placeholder-initiative">' + split[2] + image + '</span>';
-      } else if (type == "action" && split.length == 4) {
-        image = '<img  src="./assets/images/action/' + split[2] + '/' + split[3] + '.svg" class="icon ghs-svg">';
-        replace = '<span class="placeholder-perk">' + image + value + '</span>';
-      } else if (type == "items" && split.length == 4) {
-        image = '<img  src="./assets/images/items/' + split[2] + '/' + split[3] + '.svg" class="icon ghs-svg">';
-        replace = '<span class="placeholder-item-slot">' + image + value + '</span>';
-      } else if (type == "card" && split.length == 3) {
-        let card = split[2]
-        let cardValue = "";
-        if (card.split(':').length > 1) {
-          cardValue = '<span class="card-value">' + card.split(':')[1] + '</span>';
-          card = card.split(':')[0];
-        }
-        image = '<img class="icon ghs-svg" src="./assets/images/action/card/' + card + '.svg">';
-        let cardOverlay = '<img class="card-overlay" src="./assets/images/action/card/overlay/' + card + '.svg">';
-        replace = '<span class="placeholder-effect placeholder-card">' + image + cardOverlay + cardValue + '</span>';
-      } else if (type == "attackmodifier" && split.length == 3) {
-        image = '<img src="./assets/images/attackmodifier/icons/' + split[2] + '.png" class="icon">';
-        replace = '<span class="placeholder-attackmodifier">' + image + '</span>';
-      } else if (type == "characterIcon" && split.length == 3) {
-        let characterName = split[2];
-        image = '<img src="' + gameManager.characterManager.characterIcon(characterName) + '">';
-        replace = '<span class="placeholder-character-icon">' + image + '</span>';
-      } else if (type == "characterToken" && split.length == 3) {
-        let characterName = split[2];
-        image = '<img src="' + gameManager.characterManager.characterIcon(characterName) + '">';
-        replace = '<span class="placeholder-character-token" style="background-color:' + gameManager.characterManager.characterColor(characterName) + '">' + image + '</span>';
-      } else if (type == "mapMarker" && split.length == 3) {
-        replace = '<span class="map-marker">' + split[2] + '</span>';
-      } else if (type == "objectiveMarker" && split.length == 3) {
-        replace = '<span class="objective-marker">' + split[2] + '</span>';
-      } else if (type == "target" && split.length == 2) {
-        image = '<img  src="./assets/images/fh/action/target.svg" class="icon ghs-svg">';
-        replace = '<span class="placeholder-action">' + image + '</span>';
-      } else if (type == "damage") {
-        image = '<img  src="./assets/images/fh/action/damage.svg" class="icon ghs-svg">';
-        replace = '<span class="damage">' + image + value + '</span>';
-      } else if (type == "loot" && split.length == 4) {
-        image = '<img  src="./assets/images/' + split[3] + '-player.svg" class="icon">';
-        replace = '<span class="placeholder-player">' + image + '</span>';
-      } else {
-        replace = settingsManager.getLabel(label.split(':')[0], label.split(':').splice(1).map((arg) =>
-          applyFhPlaceholder(settingsManager.getLabel(arg), placeholder, relative)
-        )) + image;
       }
 
       return replace;
