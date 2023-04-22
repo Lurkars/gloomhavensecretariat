35fc3e9d44d08702d0ae1641c1cb0f06a53463f5
Lurkars <Lurkars@users.noreply.github.com>
2023-04-22 14:13:01 +0000

    deploy: d08aefb4c8f5c53085297107e588681ee42e64ed

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1017,7 +1017,7 @@ ${error.stack}`;
   };
 
   // bazel-out/k8-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "15.2.7";
+  var SW_VERSION = "15.2.8";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
