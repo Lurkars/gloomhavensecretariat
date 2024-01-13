a87b9668b3a97d1b4d8d9623eed5d262dd2ddb57
Lurkars <Lurkars@users.noreply.github.com>
2024-01-13 13:56:20 +0000

    deploy: 87421e2d8272a2f31eb5f68761c145c459c27cc1

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1032,7 +1032,7 @@ ${error.stack}`;
   };
 
   // bazel-out/darwin_arm64-fastbuild-ST-2e5f3376adb5/bin/packages/service-worker/worker/src/debug.mjs
-  var SW_VERSION = "17.0.8";
+  var SW_VERSION = "17.0.9";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
