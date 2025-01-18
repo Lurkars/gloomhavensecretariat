dbcc86f370eefe7c02aee70d842f97882862cb94
Lurkars <git@8lh.de>
2025-01-18 10:57:19 +0100

    address #655

--- src/app/game/businesslogic/BattleGoalManager.ts
+++ src/app/game/businesslogic/BattleGoalManager.ts
@@ -20,6 +20,10 @@ export class BattleGoalManager {
     return gameManager.editionData.filter((editionData) => settingsManager.settings.editions.indexOf(editionData.edition) != -1 && (gameManager.game.edition && gameManager.editionRules(editionData.edition) || gameManager.game.battleGoalEditions.indexOf(editionData.edition) != -1)).flatMap((editionData) => editionData.battleGoals.filter((battleGoal) => !filtered || !this.game.filteredBattleGoals || this.game.filteredBattleGoals.length == 0 || !this.game.filteredBattleGoals.find((identifier) => battleGoal.edition == identifier.edition && battleGoal.name == identifier.name))).filter((battleGoal, index, self) => aliases || !battleGoal.alias || !self.find((other) => battleGoal.alias && other.edition == battleGoal.alias.edition && other.name == battleGoal.alias.name));
   }
 
+  getUnrevealedBattleGoals(characterFilter: Character | undefined = undefined): BattleGoal[] {
+    return this.getBattleGoals().filter((battleGoal) => !this.game.figures.find((figure) => figure instanceof Character && (!characterFilter || figure != characterFilter) && figure.battleGoals && figure.battleGoals.find((identifier) => battleGoal.edition == identifier.edition && battleGoal.name == identifier.name)));
+  }
+
   getBattleGoalsForEdition(edition: string): BattleGoal[] {
     return gameManager.editionData.filter((editionData) => editionData.edition == edition || gameManager.editionExtensions(edition).indexOf(edition) != -1).flatMap((editionData) => editionData.battleGoals);
   }
@@ -29,18 +33,20 @@ export class BattleGoalManager {
   }
 
   drawBattleGoal(character: Character, splice: boolean = false) {
-    let battleGoals = this.getBattleGoals().filter((battleGoal) => !this.game.figures.find((figure) => figure instanceof Character && figure.battleGoals && figure.battleGoals.find((identifier) => battleGoal.edition == identifier.edition && battleGoal.name == identifier.name)));
+    let battleGoals = this.getUnrevealedBattleGoals();
 
     if (gameManager.trialsManager.apply && gameManager.trialsManager.activeTrial('fh', 360)) {
       battleGoals = battleGoals.filter((battleGoal) => battleGoal.checks == 2);
     }
 
-    let battleGoal = battleGoals[Math.floor(Math.random() * battleGoals.length)];
-    character.battleGoals = character.battleGoals || [];
-    if (splice && character.battleGoals.length > 2) {
-      character.battleGoals.splice(character.battleGoals.length - 2, 0, new Identifier(battleGoal.name, battleGoal.edition));
-    } else {
-      character.battleGoals.push(new Identifier(battleGoal.name, battleGoal.edition));
+    if (battleGoals.length) {
+      let battleGoal = battleGoals[Math.floor(Math.random() * battleGoals.length)];
+      character.battleGoals = character.battleGoals || [];
+      if (splice && character.battleGoals.length > 2) {
+        character.battleGoals.splice(character.battleGoals.length - 2, 0, new Identifier(battleGoal.name, battleGoal.edition));
+      } else {
+        character.battleGoals.push(new Identifier(battleGoal.name, battleGoal.edition));
+      }
     }
   }
 
