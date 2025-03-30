58094d946d46f7594430def2f794e4e0cd7c1c5c
Lurkars <git@8lh.de>
2025-03-30 13:10:19 +0200

    automatically sort area attacks by hex coordinates

--- scripts/sort/sorter/action.mjs
+++ scripts/sort/sorter/action.mjs
@@ -6,5 +6,36 @@ export const sortAction = function (action) {
         action.subActions = action.subActions.sort((subAction) => sortAction(subAction));
     }
 
+    // sort area effects by coordinates
+    if (action.type === 'area' && action.value) {
+        let hexes = [];
+
+        action.value.split('|').forEach((hexString) => {
+            let groups = new RegExp(/^\((\d+),(\d+),(active|target|conditional|ally|blank|enhance|invisible)(\:(\w*))?\)$/).exec(hexString);
+            if (groups == null) {
+                return null;
+            }
+
+            let actionHex = {};
+            actionHex.x = +groups[1];
+            actionHex.y = +groups[2];
+            actionHex.type = groups[3];
+            if (groups.length > 5 && groups[5]) {
+                actionHex.value = groups[5];
+            }
+            hexes.push(actionHex)
+        })
+
+        hexes.sort((a, b) => {
+            if (a.x == b.x) {
+                return a.y - b.y;
+            }
+
+            return a.x - b.x;
+        })
+
+        action.value = hexes.map((actionHex) => "(" + actionHex.x + "," + actionHex.y + "," + actionHex.type + (actionHex.value ? ":" + actionHex.value : "") + ")").join('|');
+    }
+
     return sortObjectKeys(action, 'type', 'value', 'valueType', 'valueObject', 'small', 'subActions');
 }
