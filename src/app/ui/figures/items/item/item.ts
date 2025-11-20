4910c2e011fcf58f862fef9dca76067eafe21229
ululu <mtmerlino@gmail.com>
2025-11-20 15:40:06 -0500

    fix item style

--- src/app/ui/figures/items/item/item.ts
+++ src/app/ui/figures/items/item/item.ts
@@ -53,7 +53,7 @@ export class ItemComponent implements OnInit, AfterViewInit, OnDestroy {
                 this.edition = this.item.edition;
             }
 
-            if (this.item.edition == 'fh' || this.item.edition == 'gh2e') {
+            if (this.item.edition == 'fh' || this.item.edition == 'gh2e' || this.item.edition == 'mp') {
                 this.fhStyle = true;
             }
 
