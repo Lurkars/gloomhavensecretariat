bc0ea691099d628d2979b3efbba7abe689491267
Lurkars <Lurkars@users.noreply.github.com>
2026-02-27 22:50:46 +0000

    deploy: 136142be146665b92e4a3c4b0875562f882027e4

--- ngsw-worker.js
+++ ngsw-worker.js
@@ -1285,7 +1285,7 @@ ${error.stack}`;
   };
 
   // packages/service-worker/worker/src/debug.js
-  var SW_VERSION = "21.1.5";
+  var SW_VERSION = "21.2.0";
   var DEBUG_LOG_BUFFER_SIZE = 100;
   var DebugHandler = class {
     constructor(driver, adapter2) {
