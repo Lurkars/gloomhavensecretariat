1ffeb660b5f2626284cdbe9a28ed532b2e4d8c8c
Lurkars <git@8lh.de>
2026-05-13 19:02:29 +0200

    add android release workflow (with capacitor)

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.champonthis.ghs',
+  appName: 'Gloomhaven Secretariat',
+  webDir: 'dist/gloomhavensecretariat'
+};
+
+export default config;
